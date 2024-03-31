14332a6d51c996259ef481941ab307d20fb8c531
beam <108953955+bem-boi@users.noreply.github.com>
2024-03-31 05:12:24 -0400

    lil bug that needs to be fix
    ill fix it tmralsdjkfl;ajkl;eajfl;askjsdfkl;asdjfklajsf
    
    the text wont upudate after first go

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
-from dash import Dash, dcc, html, Input, Output, callback
+from dash import Dash, dcc, html, Input, Output, callback, State
 
+import graph
 from graph import clean_agents_pick_file, clean_teams_picked_agents_file, load_agent_role_data, load_map_agent_data, \
     generate_weighted_graph, return_graph
 
@@ -12,6 +13,7 @@ cleaned_agf_file = clean_agents_pick_file('graph_data/agents_pick_rates2023.csv'
 cleaned_tpa_file = clean_teams_picked_agents_file('graph_data/teams_picked_agents2023.csv')
 agent_role_data = load_agent_role_data('graph_data/agent_roles.csv')
 map_agent_data = load_map_agent_data(cleaned_agf_file, cleaned_tpa_file, agent_role_data)
+map_agent_graph = generate_weighted_graph(map_agent_data)
 
 
 app = Dash(__name__)
@@ -48,7 +50,12 @@ def render_content(tab):
                  'fracture',
                  'bind',
                  'haven', 'all'], 'all', inline=True, id='choice2'),
-            dcc.Graph(figure={}, id='visual_graph')
+            dcc.Graph(figure={}, id='visual_graph'),
+            html.Hr(),
+            html.Div(dcc.Input(id='input_user', type='text')),
+            html.Button('Submit', id='button'),
+            html.Div(id='output-container-button',
+                     children='Enter a value and press submit')
         ])
     elif tab == 'tab-2':
         return html.Div([
@@ -70,6 +77,21 @@ def render_content(tab):
 def update_graph(choice1, choice2):
     fig = return_graph(map_agent_data, choice1, choice2)
     return fig
+
+
+@callback(
+    Output('output-container-button', 'children'),
+    [Input('choice1', 'value'),
+     Input('choice2', 'value')],
+    State('input_user', 'text'),
+    Input('button', 'n_clicks'),
+    prevent_initial_call=True)
+def update_output(choice1, choice2, input_user, n_clicks):
+    click = n_clicks
+    agents_so_far = 'The best agent to play on ' + choice2 + ' are '
+    teammate_agents = str(input_user).split(',')
+    agents_so_far += str(list(graph.best_agent_for_map(map_agent_graph, choice2, teammate_agents, choice1).keys()))
+    return agents_so_far
 
 
 if __name__ == '__main__':
